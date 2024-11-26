85e17312c505822e2cb3847c61dc5b0f3a340e5a
zaheer-abbas-g <zaheeramd8@gmail.com>
2024-11-26 14:34:21 +0500

    edit images

--- app/Http/Controllers/MultiImagesController.php
+++ app/Http/Controllers/MultiImagesController.php
@@ -2,27 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MultiImages;
 use Illuminate\Http\Request;
+use DataTables;
 
 class MultiImagesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        if($request->ajax()){
+            $multipleImages = MultiImages::select('id','image')->latest()->get();
+            return DataTables::of($multipleImages)
+                ->addIndexColumn()
+                ->addColumn('action', function ($row) {
+                    $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $row->id . '" data-original-title="Edit" class="edit btn btn-primary btn-sm editiamges"><i class="fa fa-pencil-square" aria-hidden="true"></i></a>';
+                    $btn = $btn . ' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $row->id . '" data-original-title="Delete" class="btn btn-danger btn-sm deleteiamges"><i class="fa fa-trash-o" aria-hidden="true"></i></a>';
+                    return $btn;
+                })
+                ->addColumn('image', function ($row) {
+                    $image = '<img src="' . asset('admin/images/multipleImages/' . $row->image) .'"  alt="no image found" style="width: 100px; height: 100px; object-fit: cover;" class="rounded mx-auto d-block" >';
+                    return $image;
+                })
+                ->rawColumns(['action','image'])
+                ->make(true);
+        }
         return view('admin.multipleimages');
     }
 
     public function store(Request $request)
     {
-
+        $multipleImages = new MultiImages();
         if ($request->hasFile('multipleImages')) {
-
             $images = $request->multipleImages;
-
             $data = [];
             foreach ($images as $key => $image) {
-                $data['imageNames'][$key] = $image;
+                $iamgeName  = time().'.'.$image->extension();
+                $image->move('admin/images/multipleImages', $iamgeName);
+                $data[]['image'] = $iamgeName;
             }
-            return response()->json($data);
+           foreach( $data as $key => $values){
+                $multipleImages::create($values);
+            } 
+        return response()->json(['success'=> true ,'message' => 'Multiple images are successfully inserted']);
         }
+    }
+
+    public function edit($id){
+
+        $multimages = MultiImages::findOrFail($id);
+        return response()->json(['success'=> true, 'data'=> $multimages]);
     }
 }
--- resources/views/admin/multipleimages.blade.php
+++ resources/views/admin/multipleimages.blade.php
@@ -1,116 +1,186 @@
 @extends('layouts.app')
 
 
-@section('title','Multiple Images')
+@section('title', 'Multiple Images')
 
 @section('content')
 
-<div class="container mt-3">
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h1>Multiple Images Listing
-                        <span> <a href="" class="btn btn-info btn-sm float-right"  data-toggle="modal" data-target="#exampleModalCenter">Create Images</a></span>
-                    </h1>
-                </div>
-                <div class="card-body">
-                    <table class="table table-borded data-table">
-                        <thead>
-                            <tr>
-                                <th>S.No</th>
-                                <th>Image</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            
-                        </tbody>
-                    </table>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h1>Multiple Images Listing
+                            <span> <a href="" class="btn btn-info btn-sm float-right crateImages" data-toggle="modal"
+                                    data-target="#exampleModalCenter">Create Images</a></span>
+                        </h1>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered data-table">
+                            <thead>
+                                <tr>
+                                    <th>S.No</th>
+                                    <th>Image</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLongTitle">Create Multiple Images</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            </div>
-
-        <form enctype="multipart/form-data" id="multiImagesForm">
-           
-            <div class="modal-body">
-                <div class="form-group">
-                    <label for="multipleImagesUpload">Upload images</label>
-                    <input type="file" class="form-control-file" id="multipleImages" name="multipleImages[]" multiple>
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Create Multiple Images</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary" id="Upload">Upload</button>
-            </div>
-        </form>
 
+                <form enctype="multipart/form-data" id="multiImagesForm">
+
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="multipleImagesUpload">Upload images</label>
+                            <input type="file" class="form-control-file" id="multipleImages" name="multipleImages[]"
+                                multiple>
+                        </div>
+
+                        <div class="form-group imagepreivew">
+
+                        </div>
+
+                        <div class="form-group">
+                            <img src="{{ asset('admin/images/preview.jpg') }}" id="imagepreivew" name="imagepreivew"
+                                class="rounded float-left " alt="no image found"
+                                style="width: 100px; height: 100px; object-fit: cover;">
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" id="Upload">Upload</button>
+                    </div>
+                </form>
+
+            </div>
         </div>
     </div>
-</div>
 
-<script type="text/javascript">
+    <script type="text/javascript">
+        $(document).ready(function() {
 
-        $(document).ready(function(){
-                
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+
+            // $('.crateImages').on('click', function() {
+            //     $('#imagepreivew').attr('src', '');
+            // })
+
+            /////////////////// Uploading Images ////////////////
+            $('#Upload').on('click', function(e) {
+                e.preventDefault();
+
+                var formData = new FormData($('#multiImagesForm')[0]);
+                $('#Upload').html('Uploading...');
+
+                $.ajax({
+                    ajax: {
+                        url: "{{ url('multipleimages') }}"
+                    },
+                    type: 'post',
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    success: function(response) {
+                        $('#exampleModalCenter').modal('hide');
+                        $('#Upload').html('Upload');
+                        $('#multiImagesForm')[0].reset();
+                        $('#multipleImages').val('');
+                        console.log(response);
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: "Your work has been saved",
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+
+                        table.ajax.reload();
+                    },
+                    error: function(xhr) {
+                        var error_response = JSON.parse(xhr.responseText());
+                        console.log(error_response.errors);
+                    }
+                })
+            })
+
+            ///////////////////// Multiple Images listing /////////////////////////
+            var table = $('.data-table').DataTable({
+                ajax: {
+                    url: "{{ url('multipleimages') }}"
+                },
+                processing: true,
+                serverSide: true,
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex'
+                    },
+                    {
+                        data: 'image',
+                        name: 'image'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false,
+                    }
+
+                ]
+            });
+
+
+            ////////////// edit images //////////
+            $('body').on('click', '.editiamges', function() {
+                var image_id = $(this).data('id');
+
+                $.ajax({
+                    url: "{{ url('multipleimages') }}" + '/' + image_id + '/edit',
+                    type: 'get',
+                    dataType: 'JSON',
+                    success: function(response) {
+                        console.log(response.data.image);
+                        var imagePath = "{{ asset('admin/images/multipleImages') }}";
+                        var image = imagePath + '/' + response.data.image;
+                        $('#imagepreivew').attr('src', image);
+                        $('#exampleModalCenter').modal('show');
+                        $('#exampleModalLongTitle').html('Edit Image');
+                        $('#Upload').html('Update Image');
+                    },
+                    error: function(xhr) {
+                        var error = xhr.responseText();
+                        console.log(error.errors);
                     }
                 });
 
-                var table = $('.data-table').DataTable();
-
-                /////////////////// Uploading Images ////////////////
-
-                $('#Upload').on('click',function(){
-              
-                    var formData = new FormData($('#multiImagesForm')[0]);
-                    $('#Upload').html('Uploading...');
-
-                    $.ajax({
-                        url:  "{{ url('multipleimages') }}",
-                        type:'post',
-                        data:formData,
-                        processData:false,
-                        contentType:false,
-                        success:function(response){
-                            $('#exampleModalCenter').modal('hide');
-                            $('#Upload').html('Upload');
-                            $('#multiImagesForm')[0].reset();
-                            $('#multipleImages').val('');
-                            console.log(response);
-                            Swal.fire({
-                                position: "top-end",
-                                icon: "success",
-                                title: "Your work has been saved",
-                                showConfirmButton: false,
-                                timer: 1500
-                            });
-                        },
-                        error:function(xhr){
-                            var error_response = JSON.parse(xhr.responseText());
-                            console.log(error_response.errors);
-                        }
-                    })
-                })
-
+            })
 
         });
-</script>
+    </script>
 
 @endsection
